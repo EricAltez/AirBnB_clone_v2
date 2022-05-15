e9f92bdb7f40fe89fe981ae3369f7ef2da1dddec
EricAltez <ericaltez1111@gmail.com>
2022-05-15 23:01:20 +0000

    add file 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application, update
+"""
+from cgitb import text
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Print HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """ display “C ” followed by the value of the text variable"""
+    new_text = text.replace('_', ' ')
+    return "C {}".format(new_text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ display “Python ” followed by the value of the text variable"""
+    new_text = text.replace('_', ' ')
+    return "Python {}".format(new_text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """display “n is a number” only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_temp(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def num_odd_even(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
